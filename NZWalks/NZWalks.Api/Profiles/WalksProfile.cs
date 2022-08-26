797f4cb457fbc02384de17385bfaacac519a7fee
Varun Rastogi <contactvarunrastogi@gmail.com>
2022-08-26 11:02:26 +0530

    Adding Walk  Profile to create mapping between domain and DTO

---
+++ NZWalks/NZWalks.Api/Profiles/WalksProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+
+namespace NZWalks.Api.Profiles
+{
+    public class WalksProfile:Profile
+    {
+        public WalksProfile()
+        {
+            CreateMap<Models.Domain.Walk, Models.DTO.Walk>()
+                .ReverseMap();
+            CreateMap<Models.Domain.WalkDifficulty, Models.DTO.WalkDifficulty >()
+                .ReverseMap();
+        }
+    }
+}

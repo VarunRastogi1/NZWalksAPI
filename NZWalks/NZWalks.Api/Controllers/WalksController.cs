9ce01f99239e218a79c99e97a43d7f802d7d3c4c
Varun Rastogi <contactvarunrastogi@gmail.com>
2022-08-26 11:03:50 +0530

    Adding WalksController and creating new endpoint GetAllWalks

---
+++ NZWalks/NZWalks.Api/Controllers/WalksController.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.Api.Repositories;
+
+namespace NZWalks.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WalksController : Controller
+    {
+        private readonly IWalkRepository walkRepository;
+        private readonly IMapper mapper;
+
+        public WalksController(IWalkRepository walkRepository, IMapper mapper)
+        {
+            this.walkRepository = walkRepository;
+            this.mapper = mapper;
+        }  
+        
+        [HttpGet]
+        public async Task<IActionResult> GetAllWalksAsync()
+        {
+            // Fetch data from database- domain walks
+            var walksDomain= await walkRepository.GetAllAsync();
+
+
+            //Convert domain walks to DTO walks
+            var walkDTO = mapper.Map < List<Models.DTO.Walk>>(walksDomain);
+
+
+            //Return response
+            return Ok(walkDTO);
+        }
+    }
+}

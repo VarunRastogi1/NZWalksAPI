2b90e453fb93d1fba37a8d909e1a8afe91beeae8
Varun Rastogi <contactvarunrastogi@gmail.com>
2022-08-26 10:59:52 +0530

    Adding Walk Repositories

---
+++ NZWalks/NZWalks.Api/Repositories/IWalkRepository.cs
@@ -0,0 +1,9 @@
+﻿using NZWalks.Api.Models.Domain;
+
+namespace NZWalks.Api.Repositories
+{
+    public interface IWalkRepository
+    {
+        Task<IEnumerable<Walk>> GetAllAsync();
+    }
+}
---
+++ NZWalks/NZWalks.Api/Repositories/WalkRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.Api.Data;
+using NZWalks.Api.Models.Domain;
+
+namespace NZWalks.Api.Repositories
+{
+    public class WalkRepository : IWalkRepository
+    {
+        private readonly NZWalksDbContext nZWalksDbContext;
+
+        public WalkRepository(NZWalksDbContext nZWalksDbContext)
+        {
+            this.nZWalksDbContext = nZWalksDbContext;
+        }
+        
+        public async Task<IEnumerable<Walk>> GetAllAsync()
+        {
+            return await nZWalksDbContext.Walks.ToListAsync();
+        }
+    }
+}
